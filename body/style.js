03c7bdeea02e4e893f59dbaebab6557409dc86c2
AnnCathrine23 <58133322+AnnCathrine23@users.noreply.github.com>
2020-05-26 20:27:41 +0200

    kommentar på js

--- body/style.js
+++ body/style.js
@@ -77,19 +77,19 @@ window.onclick = function(event) {
     evt.currentTarget.className += " active";
   }
 
-  // When the user clicks on div, open the popup
+  // når brugeren klikker på div, åben popoup
   function myPopupFunction1() {
     var popup1 = document.getElementById("myPopup1");
     popup1.classList.toggle("show");
   }
 
-  // When the user clicks on div, open the popup
+  // når brugeren klikker på div, åben popoup
   function myPopupFunction2() {
     var popup2 = document.getElementById("myPopup2");
     popup2.classList.toggle("show");
   }
 
-  // When the user clicks on div, open the popup
+  // når brugeren klikker på div, åben popoup
   function myPopupFunction3() {
     var popup3 = document.getElementById("myPopup3");
     popup3.classList.toggle("show");
